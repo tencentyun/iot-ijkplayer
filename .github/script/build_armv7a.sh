e7011dbf3fb1f08f860528deb3e5d85427a834ba
archurtan <archurtan@tencent.com>
2021-11-23 19:41:31 +0800

    增加arm64架构的支持

--- .github/script/build_armv7a.sh
+++ .github/script/build_armv7a.sh
@@ -5,7 +5,10 @@
 
 cd android/contrib
 ./compile-openssl.sh armv7a
+./compile-openssl.sh arm64
 ./compile-ffmpeg.sh clean
 ./compile-ffmpeg.sh armv7a
+./compile-ffmpeg.sh arm64
 cd ..
 ./compile-ijk.sh armv7a
+./compile-ijk.sh armv64
