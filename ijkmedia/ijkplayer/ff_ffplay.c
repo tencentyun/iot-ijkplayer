2710c531a07202ebfa7a4da3d181e2828ab7c32d
eagleychen <eagleychen@tencent.com>
2025-02-20 09:54:15 +0800

    update 追帧策略

--- ijkmedia/ijkplayer/ff_ffplay.c
+++ ijkmedia/ijkplayer/ff_ffplay.c
@@ -1206,94 +1206,12 @@ static void check_external_clock_speed(VideoState *is, FFPlayer *ffp) {
    }
 #endif
 
-#if 0
-//#else
     if (is->video_stream > 0 && is->videoq.nb_packets > EXTERNAL_CLOCK_MAX_FRAMES) {
-        set_clock_speed(&is->extclk, FFMIN(EXTERNAL_CLOCK_SPEED_MAX, is->extclk.speed + EXTERNAL_CLOCK_SPEED_STEP_UP));
+        set_clock_speed(&is->extclk, FFMIN(EXTERNAL_CLOCK_SPEED_MAX, is->extclk.speed + EXTERNAL_CLOCK_SPEED_STEP));
     } else {
         double speed = is->extclk.speed;
         if (speed != 1.0)
            set_clock_speed(&is->extclk, speed + EXTERNAL_CLOCK_SPEED_STEP * (1.0 - speed) / fabs(1.0 - speed));
-    }
-#endif
-    
-#if 0
-    static int temp_packet_num = 0;
-    if ((temp_packet_num > 0) || (ffp->packet_max_num > 0)) {
-        temp_packet_num = ffp->packet_max_num;
-    }else {
-        temp_packet_num = EXTERNAL_CLOCK_MAX_FRAMES;
-    }
-    
-    if (is->videoq.nb_packets > temp_packet_num) {
-        
-        int synctype = get_master_sync_type(is);
-        if (synctype == AV_SYNC_AUDIO_MASTER) {
-//            printf("speeddddddd===audio===packets===>%d\n",is->videoq.nb_packets);
-            if (ffp->audio_speed > 1) {
-                ffp_set_playback_rate(ffp, ffp->audio_speed);
-            }else {
-                ffp_set_playback_rate(ffp, EXTERNAL_CLOCK_SPEED_MAX);
-            }
-        }else {
-//            printf("speeddddddd===exter===packets===>%d\n",is->videoq.nb_packets);
-            set_clock_speed(&is->extclk, EXTERNAL_CLOCK_SPEED_MAX);
-        }
-    }else {
-        if (is->video_stream < 0 && is->audioq.nb_packets > temp_packet_num) {
-            if (ffp->audio_speed > 1) {
-                ffp_set_playback_rate(ffp, ffp->audio_speed);
-            }else {
-                ffp_set_playback_rate(ffp, EXTERNAL_CLOCK_SPEED_MAX);
-            }
-        }else {
-            ffp_set_playback_rate(ffp, 1);
-            set_clock_speed(&is->extclk, 1);
-        }
-    }
-#endif
-    if (is->video_stream > 0) {
-        if (is->videoq.nb_packets > EXTERNAL_CLOCK_MAX_FRAMES) { //if (is->videoq.nb_packets > temp_packet_num) {
-            
-            int synctype = get_master_sync_type(is);
-            if (synctype == AV_SYNC_AUDIO_MASTER) {
-                //            printf("speeddddddd===audio===packets===>%d\n",is->videoq.nb_packets);
-                if (ffp->audio_speed > 1) {
-                    ffp_set_playback_rate(ffp, ffp->audio_speed);
-                }else {
-                    ffp_set_playback_rate(ffp, EXTERNAL_CLOCK_SPEED_MAX);
-                }
-            }else {
-                //            printf("speeddddddd===exter===packets===>%d\n",is->videoq.nb_packets);
-                set_clock_speed(&is->extclk, EXTERNAL_CLOCK_SPEED_MAX);
-            }
-            
-        }else if (is->videoq.nb_packets < EXTERNAL_CLOCK_MIN_FRAMES) {
-            
-            ffp_set_playback_rate(ffp, 1);
-            double speed = is->extclk.speed;
-            if (speed != 1.0) {
-                set_clock_speed(&is->extclk, 1);
-            }
-        }
-    }
-    else {
-        //only audio stream
-        if (is->audio_stream >= 0) {
-            if (is->audioq.nb_packets > EXTERNAL_CLOCK_MAX_FRAMES) {
-                if (ffp->audio_speed > 1) {
-                    ffp_set_playback_rate(ffp, ffp->audio_speed);
-                }else {
-                    ffp_set_playback_rate(ffp, EXTERNAL_CLOCK_SPEED_MAX);
-                }
-            }else if (is->audioq.nb_packets < EXTERNAL_CLOCK_MIN_FRAMES){
-                ffp_set_playback_rate(ffp, 1);
-                double speed = is->extclk.speed;
-                if (speed != 1.0) {
-                    set_clock_speed(&is->extclk, 1);
-                }
-            }
-        }
     }
 }
 
