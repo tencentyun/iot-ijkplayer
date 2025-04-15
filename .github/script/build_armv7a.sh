9514673f5f3cf6a2781d38e13caf19f33c1ca6ee
neilyhe <80503591+neilyhe@users.noreply.github.com>
2025-04-15 19:42:00 +0800

    Update build_armv7a.sh
    test

--- .github/script/build_armv7a.sh
+++ .github/script/build_armv7a.sh
@@ -6,12 +6,9 @@
 cd android/contrib
 ./compile-openssl.sh clean
 ./compile-openssl.sh armv7a
-./compile-ffmpeg.sh clean
-./compile-ffmpeg.sh armv7a
-
-./compile-openssl.sh clean
 ./compile-openssl.sh arm64
 ./compile-ffmpeg.sh clean
+./compile-ffmpeg.sh armv7a
 ./compile-ffmpeg.sh arm64
 cd ..
 ./compile-ijk.sh armv7a
