3137e1e940ce652865bc170bdaf61348d161d7c0
neilyhe <228429803@qq.com>
2025-06-14 11:02:03 +0800

    更新编译脚本

--- .github/script/build_armv7a.sh
+++ .github/script/build_armv7a.sh
@@ -4,18 +4,15 @@
 ./init-android.sh
 
 cd android/contrib
-./compile-openssl.sh armv7a
-./compile-ffmpeg.sh clean
-./compile-ffmpeg.sh armv7a
-cd ..
-./compile-ijk.sh armv7a
-
-./init-android-openssl.sh
-./init-android.sh
-
-cd android/contrib
 ./compile-openssl.sh arm64
 ./compile-ffmpeg.sh clean
 ./compile-ffmpeg.sh arm64
 cd ..
 ./compile-ijk.sh arm64
+
+cd android/contrib
+./compile-openssl.sh armv7a
+./compile-ffmpeg.sh clean
+./compile-ffmpeg.sh armv7a
+cd ..
+./compile-ijk.sh armv7a
