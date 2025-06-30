8aed02d69a5208029955410f01fc015a3d039f4d
neilyhe <228429803@qq.com>
2025-06-30 15:30:27 +0800

    修改打包顺序

--- .github/script/build_armv7a.sh
+++ .github/script/build_armv7a.sh
@@ -4,15 +4,15 @@
 ./init-android.sh
 
 cd android/contrib
-./compile-openssl.sh arm64
-./compile-ffmpeg.sh clean
-./compile-ffmpeg.sh arm64
-cd ..
-./compile-ijk.sh arm64
-
-cd contrib
 ./compile-openssl.sh armv7a
 ./compile-ffmpeg.sh clean
 ./compile-ffmpeg.sh armv7a
 cd ..
 ./compile-ijk.sh armv7a
+
+cd contrib
+./compile-openssl.sh arm64
+./compile-ffmpeg.sh clean
+./compile-ffmpeg.sh arm64
+cd ..
+./compile-ijk.sh arm64
