c9cdd25cf3899ec657cceabe791f53a605c6df18
neilyhe <228429803@qq.com>
2025-07-28 15:42:04 +0800

    注掉和exoplayer相关的代码

--- android/ijkplayer/ijkplayer-example/src/main/java/tv/danmaku/ijk/media/example/widget/media/IjkVideoView.java
+++ android/ijkplayer/ijkplayer-example/src/main/java/tv/danmaku/ijk/media/example/widget/media/IjkVideoView.java
@@ -48,7 +48,7 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import tv.danmaku.ijk.media.exo.IjkExoMediaPlayer;
+//import tv.danmaku.ijk.media.exo.IjkExoMediaPlayer;
 import tv.danmaku.ijk.media.player.AndroidMediaPlayer;
 import tv.danmaku.ijk.media.player.IMediaPlayer;
 import tv.danmaku.ijk.media.player.IjkMediaPlayer;
@@ -532,6 +532,16 @@ public class IjkVideoView extends FrameLayout implements MediaController.MediaPl
                             break;
                     }
                     return true;
+                }
+
+                @Override
+                public boolean onInfoSEI(IMediaPlayer mp, int what, int extra, String sei_content) {
+                    return false;
+                }
+
+                @Override
+                public void onInfoAudioPcmData(IMediaPlayer mp, byte[] arrPcm, int length) {
+
                 }
             };
 
@@ -1026,11 +1036,11 @@ public class IjkVideoView extends FrameLayout implements MediaController.MediaPl
         IMediaPlayer mediaPlayer = null;
 
         switch (playerType) {
-            case Settings.PV_PLAYER__IjkExoMediaPlayer: {
-                IjkExoMediaPlayer IjkExoMediaPlayer = new IjkExoMediaPlayer(mAppContext);
-                mediaPlayer = IjkExoMediaPlayer;
-            }
-            break;
+//            case Settings.PV_PLAYER__IjkExoMediaPlayer: {
+//                IjkExoMediaPlayer IjkExoMediaPlayer = new IjkExoMediaPlayer(mAppContext);
+//                mediaPlayer = IjkExoMediaPlayer;
+//            }
+//            break;
             case Settings.PV_PLAYER__AndroidMediaPlayer: {
                 AndroidMediaPlayer androidMediaPlayer = new AndroidMediaPlayer();
                 mediaPlayer = androidMediaPlayer;
