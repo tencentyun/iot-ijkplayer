3ae7d4da2f5179acb0120c91a6bd3efd493f6c6e
neilyhe <228429803@qq.com>
2025-09-24 10:39:54 +0800

    脚本修改

--- .github/script/updateValuesForSign.sh
+++ .github/script/updateValuesForSign.sh
@@ -4,6 +4,9 @@ key_id=$KEY_ID_OF_SIGN
 password=$PASSWORD_OF_SIGN
 cd android/ijkplayer
 root_path=$(pwd)
-sed -i "" 's#MY_KEY_ID#'$key_id'#g' gradle.properties
-sed -i "" 's#MY_PASSWORD#'$password'#g' gradle.properties
-sed -i "" 's#MY_KEY_RING_FILE#'$root_path'/secring.gpg#g' gradle.properties
+sed -i 's#MY_KEY_ID#'$key_id'#g' gradle.properties
+sed -i 's#MY_PASSWORD#'$password'#g' gradle.properties
+sed -i 's#MY_KEY_RING_FILE#'$root_path'/secring.gpg#g' gradle.properties
+
+sed -i 's#MY_MAVEN_USERNAME#'$IOT_SONATYPE_USERNAME'#g' gradle.properties
+sed -i 's#MY_MAVEN_PASSWORD#'$IOT_SONATYPE_PASSWORD'#g' gradle.properties
